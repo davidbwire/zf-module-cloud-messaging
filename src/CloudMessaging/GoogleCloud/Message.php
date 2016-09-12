d1fe1b44bfb69c25ece3e01300903eeeff0cc787
David Bwire <israelbwire@gmail.com>
2016-09-12 23:14:18 +0300

    rename send() to sendToRegistrationIds() and add sendToTopic method

--- src/CloudMessaging/GoogleCloud/Message.php
+++ src/CloudMessaging/GoogleCloud/Message.php
@@ -43,6 +43,18 @@ class Message
 
     /**
      *
+     * @var array
+     */
+    protected $responseData;
+
+    /**
+     *
+     * @var int
+     */
+    protected $responseCode;
+
+    /**
+     *
      * @param string $apiKey
      */
     public function __construct($apiKey, array $cloudMessagingConfig)
@@ -58,7 +70,7 @@ class Message
      * @return false|MulticastResponse
      * @throws \RuntimeException
      */
-    public function send(array &$registrationIds = [],
+    public function sendToRegistrationIds(array &$registrationIds = [],
             array &$gcmDataOptions = ["title" => "You have a message."],
             &$userIds = [])
     {
@@ -94,31 +106,7 @@ class Message
             }
         }
 
-        return $this->sendViaCurl($gcmConfig);
-    }
-
-    private function sendViaCurl(array &$gcmConfig)
-    {
-        // prepare the headers
-        $headers = [
-            'Authorization: key=' . $this->apiKey,
-            'Content-Type: application/json'
-        ];
-
-        $curlHandle = curl_init();
-
-
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
-
-        curl_setopt($curlHandle, CURLOPT_URL,
-                $this->cloudMessagingConfig['gcm_endpoint']);
-
-        curl_setopt($curlHandle, CURLOPT_POST, true);
-        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-
-        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, json_encode($gcmConfig));
+        $curlHandle = $this->prepareCurlTransport($gcmConfig);
 
         $result = curl_exec($curlHandle);
 
@@ -157,6 +145,113 @@ class Message
             return $this->processMultiCastResponseData($aResponseData,
                             $responseCode, $gcmConfig);
         }
+    }
+
+    /**
+     *
+     * @param type $topic
+     * @param array $gcmDataOptions
+     * @return true|false|null
+     * @throws \RuntimeException
+     */
+    public function sendToTopic($topic,
+            array &$gcmDataOptions = ["title" => "You have a message."])
+    {
+        $gcmConfig = $this->cloudMessagingConfig['gcm_config'];
+        // unset the reg_id key
+        unset($gcmConfig['registration_ids']);
+        // set the topic
+        $gcmConfig['to'] = $topic;
+
+        // remove actions key if it's not specified
+        if (!isset($gcmDataOptions['actions']) ||
+                empty($gcmDataOptions['actions'])) {
+            if (array_key_exists('actions', $gcmConfig['data'])) {
+                unset($gcmConfig['data']['actions']);
+            }
+        }
+        // add or replace data on  $gcmConfig
+        if (!empty($gcmDataOptions)) {
+            foreach ($gcmDataOptions as $key => $value) {
+                $gcmConfig['data'][$key] = $value;
+            }
+        }
+
+        $curlHandle = $this->prepareCurlTransport(
+                $gcmConfig);
+
+        $result = curl_exec($curlHandle);
+
+        if ($result === false) {
+            // an error occured
+            $curlError = curl_error($curlHandle);
+            curl_close($curlHandle);
+            throw new \RuntimeException('Curl error: ' . $curlError);
+        }
+
+        $responseCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+        // set the response code
+
+        $this->responseCode = $responseCode;
+
+        // close connection
+        curl_close($curlHandle);
+
+        $aResponseData = json_decode($result, true, 512, JSON_BIGINT_AS_STRING);
+
+        if ($aResponseData === null) {
+            // decoding failed
+            return null;
+        }
+
+        // set the response data
+
+        $this->responseData = $aResponseData;
+
+
+        // check code
+        if ($responseCode === 200) {
+            return true;
+        } else {
+            /**
+             * GCM error occured
+             *
+             * 400 	Only applies for JSON requests. Indicates that the request could not be parsed as JSON, or it contained invalid fields (for instance, passing a string where a number was expected). The exact failure reason is described in the response and the problem should be addressed before the request can be retried.
+             * 401 	There was an error authenticating the sender account.
+             * 5xx 	Errors in the 500-599 range (such as 500 or 503) indicate that there was an internal error in the GCM connection server while trying to process the request, or that the server is temporarily unavailable (for example, because of timeouts). Sender must retry later, honoring any Retry-After header included in the response. Application servers must implement exponential back-off.
+             */
+            return false;
+        }
+    }
+    /**
+     *
+     * @param array $gcmConfig
+     * @return mixed
+     */
+    private function prepareCurlTransport(array &$gcmConfig)
+    {
+        // prepare the headers
+        $headers = [
+            'Authorization: key=' . $this->apiKey,
+            'Content-Type: application/json'
+        ];
+
+        $curlHandle = curl_init();
+
+
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($curlHandle, CURLOPT_URL,
+                $this->cloudMessagingConfig['gcm_endpoint']);
+
+        curl_setopt($curlHandle, CURLOPT_POST, true);
+        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, json_encode($gcmConfig));
+
+        return $curlHandle;
     }
 
     /**
